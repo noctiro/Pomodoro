ab64e108cb351eeb73be45eec4c0dec09931e193
Noctiro <49872497+Noctiro@users.noreply.github.com>
2025-07-28 19:13:52 +0800

    Folia support

--- src/main/java/org/encinet/pomodoro/ui/UIManager.java
+++ src/main/java/org/encinet/pomodoro/ui/UIManager.java
@@ -1,5 +1,4 @@
 package org.encinet.pomodoro.ui;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.encinet.pomodoro.Pomodoro;
@@ -7,27 +6,27 @@ import org.encinet.pomodoro.Pomodoro;
 public class UIManager {
     public void openPresetMainUI(Player player) {
         Inventory inventory = PresetMainUI.create(player);
-        Bukkit.getScheduler().runTask(Pomodoro.getInstance(), () -> player.openInventory(inventory));
+        player.getScheduler().run(Pomodoro.getInstance(), (task) -> player.openInventory(inventory), null);
     }
 
     public void openTimerUI(Player player) {
         Inventory inventory = TimerUI.create(player, Pomodoro.getInstance().getPomodoroManager());
         if (inventory != null) {
-            Bukkit.getScheduler().runTask(Pomodoro.getInstance(), () -> player.openInventory(inventory));
+            player.getScheduler().run(Pomodoro.getInstance(), (task) -> player.openInventory(inventory), null);
         }
     }
 
     public void openPresetEditingUI(Player player, String presetKey) {
         Inventory inventory = PresetEditingUI.create(player, presetKey);
         if (inventory != null) {
-            Bukkit.getScheduler().runTask(Pomodoro.getInstance(), () -> player.openInventory(inventory));
+            player.getScheduler().run(Pomodoro.getInstance(), (task) -> player.openInventory(inventory), null);
         }
     }
 
     public void openIconSelectionUI(Player player, String presetKey) {
         Inventory inventory = IconSelectionUI.create(player, presetKey);
         if (inventory != null) {
-            Bukkit.getScheduler().runTask(Pomodoro.getInstance(), () -> player.openInventory(inventory));
+            player.getScheduler().run(Pomodoro.getInstance(), (task) -> player.openInventory(inventory), null);
         }
     }
 }
